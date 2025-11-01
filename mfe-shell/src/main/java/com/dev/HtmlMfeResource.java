313b190a88e28a04aa6c273dc1dd218b067fe2d9
Florin Sofinet <sofinetflorin@gmail.com>
2025-11-01 13:19:31 +0000

    update turbo with master

--- mfe-shell/src/main/java/com/dev/HtmlMfeResource.java
+++ mfe-shell/src/main/java/com/dev/HtmlMfeResource.java
@@ -3,8 +3,7 @@ package com.dev;
 
 
 import htmlflow.HtmlFlow;
-import htmlflow.HtmlMfe;
-import htmlflow.HtmlMfeConfig;
+import htmlflow.HtmlView;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,8 +11,6 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.xmlet.htmlapifaster.EnumTypeScriptType;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Path("/mfe")
 public class HtmlMfeResource {
@@ -21,8 +18,7 @@ public class HtmlMfeResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public Response getHtml() {
-        List<HtmlMfeConfig> htmlMfeConfigList = new ArrayList<>();
-        HtmlMfe mfe = HtmlFlow.mfe(htmlMfeConfigList , page -> {
+        HtmlView<?> mfe = HtmlFlow.ViewFactory.builder().mfeEnabled(true).build().view((page -> {
             page.html()
                     .head()
                     .script().attrType(EnumTypeScriptType.MODULE).attrSrc("https://cdn.jsdelivr.net/npm/@hotwired/turbo@latest/dist/turbo.es2017-esm.min.js").__()
@@ -47,43 +43,10 @@ public class HtmlMfeResource {
                                 .custom("turbo-frame").addAttr("id", "test_transaction_frame").addAttr("src", "http://localhost:8084/order/history").__()
                         .__()
                     .__();
-        });
+        }));
 
         String html = mfe.render();
         return Response.ok(html).build();
 
     }
 }
-
-//ElementBase --->
-//default T mfe(MfeConfiguration data) {
-//    T self = this.self();
-//    this.getVisitor().visitMfe(self, data);
-//    return self;
-//}
-//
-//ElementVIsitorBase --->
-//public abstract <E extends Element> void visitMfe(E element, MfeConfiguration data);
-//
-//GeneratorConstants --->
-//public static final ClassName mfeConfiguration = ClassName.get(CLASS_PACKAGE, "MfeConfiguration");
-//
-//New class MfeConfiguration --->
-//        package org.xmlet.htmlapifaster;
-//
-//
-//public interface MfeConfiguration {
-//    String getMFE_URL_RESOURCE();
-//
-//    String getMFE_ELEMENT_NAME();
-//
-//    String getMFE_LISTENING_EVENT_NAME();
-//
-//    String getMFE_TRIGGERS_EVENT_NAME();
-//
-//    String getMFE_SCRIPT_NAME();
-//
-//    String getMFE_CSS_NAME();
-//
-//    boolean isMFE_MULTIPLE_BUTTONS();
-//}
