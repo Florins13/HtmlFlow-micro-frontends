f6b8af72ec17ec42e68cc4993c261a5c58accede
Florin Sofinet <sofinetflorin@gmail.com>
2025-11-02 15:48:37 +0000

    update shell resource of htmx

--- mfe-shell/src/main/java/com/dev/HtmlMfeResource.java
+++ mfe-shell/src/main/java/com/dev/HtmlMfeResource.java
@@ -3,7 +3,6 @@ package com.dev;
 
 
 import htmlflow.HtmlFlow;
-import htmlflow.HtmlMfeConfig;
 import htmlflow.HtmlView;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -23,8 +22,7 @@ public class HtmlMfeResource {
             page.html()
                     .head()
                     .script().attrSrc("https://unpkg.com/htmx.org@1.9.10").__()
-                    .script().attrSrc("https://unpkg.com/htmx.org/dist/ext/sse.js").addAttr("crossorigin", "anonymous").__()
-                    // Reference JS file in META-INF/resources/main.js
+//                    .script().attrSrc("https://unpkg.com/htmx.org/dist/ext/sse.js").addAttr("crossorigin", "anonymous").__()
                     .__()
                         .body()
                         .div().addAttr("style", "display: flex; justify-content: center;height: 100px;border: blue 1px solid;")
@@ -35,22 +33,20 @@ public class HtmlMfeResource {
                                 .custom("micro-frontend").addAttr("mfe-name", "mfe1").addAttr("hx-get", "http://localhost:8081/bikes").addAttr("hx-trigger", "load, triggerBikeEvent from:body").addAttr("hx-swap", "innerHTML").__()
                                     .script().attrSrc("http://localhost:8081/js/mfe-bikes.js").attrType(EnumTypeScriptType.MODULE).__()
                                     .link().attrHref("http://localhost:8081/css/style.css").attrRel(EnumRelType.STYLESHEET).__()
-//                                .mfe(mfeBike).__()
+                                .__()
                             .div().addAttr("style", "width: 20%;border: red 1px solid; margin: 20px")
                                 .custom("micro-frontend").addAttr("mfe-name", "mfe2").addAttr("hx-get", "http://localhost:8083/cart").addAttr("hx-trigger", "load, triggerCartEvent from:body").addAttr("hx-swap", "innerHTML").__()
                                     .script().attrSrc("http://localhost:8083/mfe-cart.js").attrType(EnumTypeScriptType.MODULE).__()
                                     .link().attrHref("http://localhost:8083/style.css").attrRel(EnumRelType.STYLESHEET).__()
-//                                .mfe(mfeCart).__()
+                                .__()
                         .__()
                         .div()
                             .div().addAttr("style", "border: green 1px solid; margin: 20px")
                                 .custom("micro-frontend").addAttr("mfe-name", "mfe3").addAttr("hx-get", "http://localhost:8084/order/history").addAttr("hx-trigger", "load, triggerOrderEvent from:body").addAttr("hx-swap", "innerHTML").__()
                                     .script().attrSrc("http://localhost:8084/mfe-order.js").attrType(EnumTypeScriptType.MODULE).__()
-//                                .mfe(mfeOrder).__()
                         .__()
                     .div().addAttr("style", "display: flex; justify-content: center;height: 50px;border: yellow 1px solid;")
                     // we he to change backend for this to work since htmx supports, CORS origin issues
-//                        .div().mfe(mfeStream).__()
 //                            .custom("micro-frontend").addAttr("hx-ext","sse").addAttr("sse-connect", "http://localhost:8080/html-chunked/stream").addAttr("sse-event", "message").addAttr("hx-swap", "beforeend").__()
                     .__()
                     .__();
